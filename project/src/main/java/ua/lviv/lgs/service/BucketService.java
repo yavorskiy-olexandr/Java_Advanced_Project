ca6d37792a94cf9b7d390d90805970076c18973d
Alex <sanyok6969@gmail.com>
2022-09-19 18:25:33 +0300

    Added logger to Service

--- project/src/main/java/ua/lviv/lgs/service/BucketService.java
+++ project/src/main/java/ua/lviv/lgs/service/BucketService.java
@@ -2,6 +2,8 @@ package ua.lviv.lgs.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +12,22 @@ import ua.lviv.lgs.domain.Bucket;
 
 @Service
 public class BucketService {
+	private Logger logger = LoggerFactory.getLogger(BucketService.class);
 	@Autowired
 	private BucketRepository bucketRepository;
 
 	public List<Bucket> getAll() {
+		logger.info("Get all buckets items");
 		return bucketRepository.findAll();
 	}
 
 	public void delete(Bucket bucket) {
+		logger.info("Delete bucket item {} : " + bucket);
 		bucketRepository.delete(bucket);
 	}
 
 	public Bucket add(Bucket bucket) {
+		logger.info("Create new bucket item {}: " + bucket);
 		return bucketRepository.save(bucket);
 	}
 }
--- project/src/main/java/ua/lviv/lgs/service/UserService.java
+++ project/src/main/java/ua/lviv/lgs/service/UserService.java
@@ -1,5 +1,7 @@
 package ua.lviv.lgs.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import ua.lviv.lgs.domain.UserRole;
 
 @Service
 public class UserService {
+	private Logger logger = LoggerFactory.getLogger(BucketService.class);
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -17,6 +20,8 @@ public class UserService {
 	private PasswordEncoder bCryptPasswordEncoder;
 	
 	public void save(User user) {
+		logger.info("Register new user {} : " + user);
+		
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPasswordConfirm()));
 		user.setRole(UserRole.ROLE_USER);
@@ -24,6 +29,7 @@ public class UserService {
 	}
     
     public User findByEmail(String email) {
+		logger.info("Get user {} by email: " + email);
     	return userRepository.findByEmail(email).get();
     }
 }
