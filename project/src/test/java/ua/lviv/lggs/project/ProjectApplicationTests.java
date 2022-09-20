cd12718657d234d8dff472ee3c6d4a4a9d3e1be4
Alex <sanyok6969@gmail.com>
2022-09-20 20:09:58 +0300

    no message

--- project/src/test/java/ua/lviv/lggs/project/ProjectApplicationTests.java
+++ project/src/test/java/ua/lviv/lggs/project/ProjectApplicationTests.java
@@ -1,13 +1,55 @@
 package ua.lviv.lggs.project;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ProjectApplicationTests {
+import ua.lviv.lgs.Application;
+import ua.lviv.lgs.dao.UserRepository;
+import ua.lviv.lgs.domain.User;
+import ua.lviv.lgs.domain.UserRole;
+import ua.lviv.lgs.service.UserService;
 
+//@SpringBootTest(classes=Application.class)
+@ContextConfiguration(classes=Application.class)
+//@WebMvcTest(MyController.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ProjectApplicationTests {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		List<User> users = userRepository.findAll();
+		assertThat(users, hasSize(0));
+
+		User user = new User(1,"1", "1", "1", UserRole.ROLE_USER, "1");
+		user.setEmail("1@gmail.com");
+		user.setFirstName("1");
+		user.setLastName("1");
+		user.setPassword("1");
+		user.setPasswordConfirm("1");
+		user.setRole(UserRole.ROLE_USER);
+
+		userService.save(user);
+
+		users = userRepository.findAll();
+		assertThat(users, hasSize(1));
 	}
 
 }
+
+
